f833157631288712cb90d8e14f5792a591e26c97
AdarshJha <adarsh2005j@gmail.com>
2024-03-14 19:22:34 +0530

    connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
-import express from 'express'
-
+import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config'
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello!!")
-})
+mongoose
+  .connect(`${process.env.MONGO_DB_URL}/Blog`)
+  .then(() => {
+    console.log("MongoDB Connected");
+  }).catch((error) => console.log(error))
+
+app.get("/", (req, res) => {
+  res.send("Hello!!");
+});
 
 app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-})
+  console.log("Server is running on port 8000");
+});
