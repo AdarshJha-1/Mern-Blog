9e66772379c8cac860b55066656164e9b1ddafd2
AdarshJha <adarsh2005j@gmail.com>
2024-03-15 14:11:11 +0530

    add functionality to the sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,47 +1,110 @@
-import { Link } from "react-router-dom";
-import {Button, Label, TextInput} from 'flowbite-react'
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setError("Please fill out all field");
+    }
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success === false) {
+        return setError(data.message);
+      }
+      setIsLoading(false);
+      if (response.ok) {
+       navigate('/sign-in')
+      }
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex flex-col md:flex-row md:items-center p-3 max-w-3xl mx-auto gap-5">
         <div className="flex-1">
-          <Link
-            to={"/"}
-            className="text-4xl font-bold dark:text-white"
-          >
+          <Link to={"/"} className="text-4xl font-bold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Adarsh's
             </span>
             Blog
           </Link>
           <p className="text-sm mt-5">
-          This is a demo project. You can sign up with your email and password or with Google.
+            This is a demo project. You can sign up with your email and password
+            or with Google.
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4  ">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="">
-              <Label value="Your username"/>
-              <TextInput type="text" placeholder="Username" id="username"/>
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
-              <Label value="Your email"/>
-              <TextInput type="email" placeholder="name@company.com" id="email"/>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
-              <Label value="Your password"/>
-              <TextInput type="password" placeholder="Password" id="password"/>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone={'purpleToPink'} type="submit">
-              Sign Up
+            <Button
+              gradientDuoTone={"purpleToPink"}
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <Spinner size={'sm'} /> <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to={'/sign-in'} className="text-blue-500">
+            <Link to={"/sign-in"} className="text-blue-500">
               Sign In
             </Link>
           </div>
+          {error && (
+            <Alert className="mt-5" color={"failure"}>
+              {error}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
