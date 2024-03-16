9fd107d8e75c4d525348d2b3052787b33a933a2a
AdarshJha <adarsh2005j@gmail.com>
2024-03-16 22:08:36 +0530

    update header component with user data

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
-import { PersistGate } from "redux-persist/es/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
